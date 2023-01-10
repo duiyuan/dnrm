f88689f00114752f3c2e184a284944a8431a31ed
wangshangwen <shangwenwang20@gmail.com>
2023-01-10 10:49:26 +0800

    chore: rename dnrm to dxnrm

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'dnrm'
+const libraryName = 'dxnrm'
 
 export default {
   input: `src/${libraryName}.ts`,
